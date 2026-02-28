a768040f18900d468499b09d46a09c5f9f647d82
Guilherme_Zimpeck <19605737728@cefet-rj.br>
2026-02-28 18:44:50 -0300

    Lista_1

---
+++ Lista 1/Questão 1/cria_numero.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int X, Y, criaNumero;
+
+    printf("digite o valor de X: \n");
+    scanf("%d", &X);
+    printf("digite o valor de Y: \n");
+    scanf("%d", &Y);
+
+    criaNumero = X * 100 + Y;
+    printf("o numero criado foi: %d",criaNumero);
+}
